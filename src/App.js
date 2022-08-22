b1e8f92952e2e0140d8a6eb2108239d5c4cff98d
Ryan <santoryan1408@gmail.com>
2022-08-22 19:30:59 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './style/style.css';
+import * as React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Preload from './components/preload/index';
 import Register from './components/preload/register';
-import Login from  './components/preload/login';
+import Login from './components/preload/login';
 import Dashboard from './components/dashboard';
 
 function App() {
   return (
-    <div className="App">
-      
-      <Routes>
-        <Route path='/' element={<Preload/>}/>
-        <Route path='signin' element={<Register/>}/>
-        <Route path='login' element={<Login/>}/>
-        <Route path='dashboard' element={<Dashboard/>}/>
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Preload />} />
+          <Route path='signin' element={<Register />} />
+          <Route path='login' element={<Login />} />
+          <Route path='dashboard' element={<Dashboard />} />
+        </Routes>
+      </div>
   );
 }
 
