ceb4b606222977ea2476bd4369f66d5881a40ddc
Ryan <santoryan1408@gmail.com>
2022-08-22 19:17:55 -0300

    Updates

---
+++ precache-manifest.2680e0b053e72596ff891d1afe649f56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcb5a78528e2c7ba3e41",
+    "url": "/login-system/static/css/main.bdd456f0.chunk.css"
+  },
+  {
+    "revision": "bcb5a78528e2c7ba3e41",
+    "url": "/login-system/static/js/main.16b7364b.chunk.js"
+  },
+  {
+    "revision": "2399644d31650087feaa",
+    "url": "/login-system/static/js/runtime~main.08a1ffc3.js"
+  },
+  {
+    "revision": "9ddf3a7eeef5b5b60fbd",
+    "url": "/login-system/static/js/2.cbe7230a.chunk.js"
+  },
+  {
+    "revision": "df1cc7efb0622d17a7c7fbf3bb707d04",
+    "url": "/login-system/index.html"
+  }
+];
